34d84f20590c3d93c7917d772e4b1c2b24142d99
FLOCK4H <161654571+FLOCK4H@users.noreply.github.com>
2024-03-17 11:57:35 +0000

    Update detect_os.py

--- tools/detect_os.py
+++ tools/detect_os.py
@@ -1,7 +1,8 @@
 # detect_os.py
 import os
+import gc
 
-def detect_os_by_files():
+def detect_os_by_sys():
     try:
         files = os.listdir("/")
         # macOS-specific files start with '_'
@@ -16,3 +17,30 @@ def detect_os_by_files():
     except Exception as e:
         print(f"Exception caught: {e}")
         return "Other"
+        
+def load_payload_from_file():
+    try:
+        loaded = []
+        tools_dir = os.listdir('tools')
+        payloads = ["payload.txt", "payload_mac.txt"]
+        
+        for p in payloads:
+            if p in tools_dir:
+                with open(f'tools/{p}', 'r') as f:
+                    processed_payload = ''
+                    for line in f:
+                        processed_line = line.strip().rstrip(';').replace('\\n', '\n').replace('\\t', '\t')
+                        processed_payload += processed_line
+                    loaded.append({p: processed_payload})
+                    gc.collect()
+            else:
+                print(f"{p} is missing from the tools folder.")
+                
+        if not loaded:
+            print("No payloads loaded. Ensure payload files are present in the tools folder.")
+            
+        return loaded
+    except OSError as e:
+        print(f"Failed to load payloads: {str(e)}")
+        return []
+
